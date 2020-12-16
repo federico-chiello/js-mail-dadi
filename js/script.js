320b83a29b27cfa9f980bf29257cbd20ec3edd50
Fede <chiellofederico@gmail.com>
2020-12-16 15:04:33 +0100

    Math.random giocatore

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+var giocatore = Math.floor(Math.random() * 6) + 1;
+// console.log(giocatore);
+
+document.getElementById('utente').innerHTML = giocatore;
