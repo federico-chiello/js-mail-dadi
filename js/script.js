ce0ae0bcd7a27e9804b464965bf86ed4f69cd1be
Fede <chiellofederico@gmail.com>
2020-12-16 16:27:00 +0100

    aggiunta metodi per la risoluzione

--- js/script.js
+++ js/script.js
@@ -1,25 +1,60 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// Inseriamo le variabili
+// Inseriamo le variabili dei giocatori
 var giocatore = Math.floor(Math.random() * 6) + 1;
-// console.log(giocatore);
 var computer = Math.floor(Math.random() * 6) + 1;
-// console.log(computer);
 
 document.getElementById('utente').innerHTML = giocatore;
 document.getElementById('utente-due').innerHTML = computer;
 
 
-var vincitoreUno = 'Giocatore';
-var vincitoreDue = 'Computer';
-var pareggio = 'Parità';
+// **** Primo metodo *****
+
+// variabile risultato
+var risultatoFinale = 'Parità'; // Con questo metodo riduciamo il codice perché si inserisce direttamente la terza condizione, senza scriverla sotto con else.
 
 // Inseriamo le istruzioni condizionali per realizzare il gioco e fare vincere il giocatore con il punteggio più alto.
 if (giocatore > computer) {
-  document.getElementById('risultato').innerHTML = 'Ha vinto: ' + vincitoreUno;
+  risultatoFinale = 'Ha vinto il giocatore';
 } else if (computer > giocatore) {
-  document.getElementById('risultato').innerHTML = 'Ha vinto: ' + vincitoreDue;
-} else if(computer == giocatore) {
-  document.getElementById('risultato').innerHTML = ' ' + pareggio;
+  risultatoFinale = 'Ha vinto il computer';
 }
+
+document.getElementById('risultato').innerHTML = risultatoFinale;
+
+
+
+// ***** Secondo metodo ******
+
+// variabile risultato
+// var risultatoFinale;
+
+// Inseriamo le istruzioni condizionali per realizzare il gioco e fare vincere il giocatore con il punteggio più alto.
+// if (giocatore > computer) {
+//   risultatoFinale = 'Ha vinto il giocatore';
+// } else if (computer > giocatore) {
+//   risultatoFinale = 'Ha vinto il computer';
+// } else {
+//   risultatoFinale = 'Parità';
+// }
+//
+// document.getElementById('risultato').innerHTML = risultatoFinale;
+
+
+
+// ***** Terzo metodo ****** (CODICE TROPPO LUNGO)
+
+// variabili
+// var vincitoreUno = 'Giocatore';
+// var vincitoreDue = 'Computer';
+// var pareggio = 'Parità';
+//
+// // Inseriamo le istruzioni condizionali per realizzare il gioco e fare vincere il giocatore con il punteggio più alto.
+// if (giocatore > computer) {
+//   document.getElementById('risultato').innerHTML = 'Ha vinto: ' + vincitoreUno;
+// } else if (computer > giocatore) {
+//   document.getElementById('risultato').innerHTML = 'Ha vinto: ' + vincitoreDue;
+// } else {
+//   document.getElementById('risultato').innerHTML = ' ' + pareggio;
+// }
